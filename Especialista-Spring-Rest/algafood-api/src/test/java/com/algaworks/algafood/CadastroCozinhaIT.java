fb3d8468d5747aa7f1d00edc841b55ccd25f6f32
Morcego <vinicius.templario@gmail.com>
2022-07-12 21:41:55 -0300

    Implementando o callback do Flyway nos testes

--- Especialista-Spring-Rest/algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ Especialista-Spring-Rest/algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -4,9 +4,11 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -23,11 +25,16 @@ public class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 	
+	@Autowired 
+	private Flyway flyway;
+	
 	@Before
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate();
 	}
 	
 	@Test
@@ -52,5 +59,18 @@ public class CadastroCozinhaIT {
 			.body("nome", hasItems("Indiana", "Tailandesa"));
 		
 	}
+	
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		given()
+			.body("{\"nome\": \"Chinesa\"}")
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+			.when()
+				.post()
+			.then()
+				.statusCode(HttpStatus.CREATED.value());
+	}
+	
 
 }
