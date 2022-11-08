29f50f181a422db7e26e3b5e4dff8d830a21e1b3
unknown <vinicius.souza@novaxs.com.br>
2022-11-08 07:43:33 -0300

    Implementando paginação e ordenação em recursos de coleção da API

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -7,6 +7,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,8 +49,14 @@ public class CozinhaController {
 	private CozinhaInputDisassembler cozinhaInputDisassembler;
 	
 	@GetMapping	
-	public List<CozinhaModel> listar(){
-		return cozinhaModelAssembler.toDomainCollector(cozinhaRepository.findAll());
+	public Page<CozinhaModel> listar(@PageableDefault(size = 2) Pageable pageable){
+		Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
+		List<CozinhaModel> cozinhasModel = 
+				cozinhaModelAssembler.toDomainCollector(cozinhasPage.getContent());
+		Page<CozinhaModel> cozinhasPageModel = 
+				new PageImpl<>(cozinhasModel, pageable, cozinhasPage.getTotalElements());
+		
+		return cozinhasPageModel;
 	} 
 	
 	//
