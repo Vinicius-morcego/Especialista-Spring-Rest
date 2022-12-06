c7178358de5d7b151f9df27a4aed2d0419a76e43
unknown <vinicius.souza@novaxs.com.br>
2022-12-06 08:39:30 -0300

    Implementando o serviço de armazenagem de fotos no disco local

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafood.domain.service;
+
+import java.io.InputStream;
+
+import org.springframework.stereotype.Service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Service
+public interface FotoStorageService {
+
+	void armazenar(NovaFoto novaFoto);
+	
+	@Getter
+	@Builder
+	class NovaFoto{
+		private String nomeArquivo;
+		private InputStream inputStream;
+	}
+}
