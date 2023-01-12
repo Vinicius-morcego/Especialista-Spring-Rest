2600752af99893a6d40cd420bc0e9e6422d58a1a
unknown <vinicius.souza@novaxs.com.br>
2023-01-12 09:03:50 -0300

    Montando modelo de representação com RepresentationModelAssembler

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -1,8 +1,5 @@
 package com.algaworks.algafood.api.controller;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 import java.util.List;
 
 import javax.validation.Valid;
@@ -56,51 +53,14 @@ public class CidadeController implements CidadeControllerOpenApi{
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public CollectionModel<CidadeModel> listar(){
-		List<CidadeModel> cidadesModel =  
-				cidadeModelAssembler.toCollectionModel(cidadeRepository.findAll());
-		
-		cidadesModel.forEach(cidadeModel ->{
-			cidadeModel.add(linkTo(methodOn(CidadeController.class)
-					.buscar(cidadeModel.getId())).withSelfRel());
-			
-			cidadeModel.add(linkTo(methodOn(CidadeController.class).listar())
-					.withRel("cidades"));
-			
-			cidadeModel.add(linkTo(methodOn(EstadoController.class)
-					.buscar(cidadeModel.getEstado().getId())).withSelfRel());
-			
-			
-		});
-		
-		
-		CollectionModel<CidadeModel> cidadesCollectionModel = 
-				CollectionModel.of(cidadesModel);
-		
-		cidadesCollectionModel.add(linkTo(CidadeController.class).withSelfRel());
-		return cidadesCollectionModel;
+		List<Cidade> todasCidades = cidadeRepository.findAll();
+		return cidadeModelAssembler.toCollectionModel(todasCidades);		
 	}	
 	
 	@GetMapping(path = "/{cidadeId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public CidadeModel buscar(@PathVariable Long cidadeId){
-		Cidade cidade = cadastroCidade.buscarOuFalhar(cidadeId);	
-		
-		CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
-		
-//		cidadeModel.add(linkTo(CidadeController.class)
-//				.slash(cidadeModel.getId()).withSelfRel());
-		cidadeModel.add(linkTo(methodOn(CidadeController.class, cidadeModel.getId())
-				.buscar(cidadeModel.getId())).withSelfRel());
-		
-//		cidadeModel.add(linkTo(CidadeController.class)
-//				.withRel("cidades"));
-		
-		cidadeModel.add(linkTo(methodOn(CidadeController.class).listar()).withRel("cidades"));
-		
-//		cidadeModel.getEstado().add(linkTo(EstadoController.class)
-//				.slash(cidadeModel.getEstado().getId()).withSelfRel());
-		cidadeModel.getEstado().add(linkTo(methodOn(EstadoController.class)
-				.buscar(cidadeModel.getEstado().getId())).withSelfRel());
-		return cidadeModel;
+		Cidade cidade = cadastroCidade.buscarOuFalhar(cidadeId);
+		return cidadeModelAssembler.toModel(cidade);
 //		Optional<Cidade> cidade = cidadeRepository.findById(cidadeId);
 //		if(cidade.isPresent()) {
 //			return ResponseEntity.ok(cidade.get());
