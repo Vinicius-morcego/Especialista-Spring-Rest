285c9eeef197c2b4ae3e0a59f9de2b6d40aaefbf
unknown <vinicius.souza@novaxs.com.br>
2023-01-24 09:00:24 -0300

    .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -25,9 +25,11 @@ import org.springframework.web.context.request.ServletWebRequest;
 import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.CidadeModel;
 import com.algaworks.algafood.api.model.CozinhaModel;
+import com.algaworks.algafood.api.model.EstadoModel;
 import com.algaworks.algafood.api.model.PedidoResumoModel;
 import com.algaworks.algafood.api.openapi.model.CidadesModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.CozinhasModelOpenApi;
+import com.algaworks.algafood.api.openapi.model.EstadosModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.LinksModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PageableModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PedidosModelOpenApi;
@@ -94,6 +96,8 @@ public class SpringFoxConfig{
 	        		  Page.class, PedidoResumoModel.class), PedidosModelOpenApi.class))
 	          .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(
 	        		  CollectionModel.class, CidadeModel.class), CidadesModelOpenApi.class))
+	          .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(
+	        		  CollectionModel.class, EstadoModel.class), EstadosModelOpenApi.class))
 	          .apiInfo(apiInfo())	          
 	          .tags(
 	        	  new Tag("Cidades", "Gerência as cidades"), 
