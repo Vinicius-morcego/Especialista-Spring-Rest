a5938e4e672dc36c4f1b5fba194d5e5649a3f721
unknown <vinicius.souza@novaxs.com.br>
2023-02-09 22:03:51 -0300

    Configurando a validação de JWT no Resource Server com a chave pública

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,18 +1,9 @@
 package com.algaworks.algafood.core.security;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -27,12 +18,5 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 			.cors().and()
 			.oauth2ResourceServer().jwt();
 	}
-	
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		var secretKey = new SecretKeySpec("sadASDsadÇLKJ~LJLKsadSADadadfasdfasdfadsfs".getBytes(), "HmacSHA256");
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
-	}
-	
 	
 }
