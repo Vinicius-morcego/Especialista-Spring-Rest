c2b01b6844f81007582c60b59327bc788fd7fc21
unknown <vinicius.souza@novaxs.com.br>
2023-02-14 12:03:52 -0300

    Cadastrando clientes OAuth2 no banco de dados e testando a emissão de tokens

--- algafood-api/src/main/resources/db/testdata/afterMigrate.sql
+++ algafood-api/src/main/resources/db/testdata/afterMigrate.sql
@@ -128,7 +128,38 @@ values ('5e7588c7-2fed-42dd-984e-e2fddea76916', 200.0, 15.2, 215.2, 'CRIADO', ut
 insert into item_pedido(quantidade, preco_unitario, preco_total, observacao, pedido_id, produto_id) values
 (2, 100.0, 100.00, 'Muito Saboroso 2', 2, 1);  
 
+insert into oauth_client_details (
+  client_id, resource_ids, client_secret, 
+  scope, authorized_grant_types, web_server_redirect_uri, authorities,
+  access_token_validity, refresh_token_validity, autoapprove
+)
+values (
+  'algafood-web', null, '$2y$12$w3igMjsfS5XoAYuowoH3C.54vRFWlcXSHLjX7MwF990Kc2KKKh72e',
+  'READ,WRITE', 'password', null, null,
+  60 * 60 * 6, 60 * 24 * 60 * 60, null
+);
 
+insert into oauth_client_details (
+  client_id, resource_ids, client_secret, 
+  scope, authorized_grant_types, web_server_redirect_uri, authorities,
+  access_token_validity, refresh_token_validity, autoapprove
+)
+values (
+  'foodanalytics', null, '$2y$12$fahbH37S2pyk1RPuIHKP.earzFmgAJJGo26rE.59vf4wwiiTKHnzO',
+  'READ,WRITE', 'authorization_code', 'http://www.foodanalytics.local:8082', null,
+  null, null, null
+);
+
+insert into oauth_client_details (
+  client_id, resource_ids, client_secret, 
+  scope, authorized_grant_types, web_server_redirect_uri, authorities,
+  access_token_validity, refresh_token_validity, autoapprove
+)
+values (
+  'faturamento', null, '$2y$12$fHixriC7yXX/i1/CmpnGH.RFyK/l5YapLCFOEbIktONjE8ZDykSnu',
+  'READ,WRITE', 'client_credentials', null, 'CONSULTAR_PEDIDOS,GERAR_RELATORIOS',
+  null, null, null
+);
 
 
 
