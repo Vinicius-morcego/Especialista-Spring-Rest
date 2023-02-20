849fb3779b3f29b48984fe7fd0ceb72062a64c50
unknown <vinicius.souza@novaxs.com.br>
2023-02-20 10:34:02 -0300

    Externalizando o KeyStore: criando um ProtocolResolver para Base64

--- algafood-api/src/main/java/com/algaworks/algafood/core/io/Base64ProtocolResolver.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/io/Base64ProtocolResolver.java
@@ -2,15 +2,24 @@ package com.algaworks.algafood.core.io;
 
 import java.util.Base64;
 
-import org.springframework.boot.context.event.ApplicationContextInitializedEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ProtocolResolver;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Base64ProtocolResolver implements ProtocolResolver, 
-		ApplicationListener<ApplicationContextInitializedEvent> {
+		ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+
+
+	@Override
+	public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+		configurableApplicationContext.addProtocolResolver(this);
+	}
 
 	@Override
 	public Resource resolve(String location, ResourceLoader resourceLoader) {
@@ -18,13 +27,8 @@ public class Base64ProtocolResolver implements ProtocolResolver,
 			byte[] decodedResource = Base64.getDecoder().decode(location.substring(7));
 			return new ByteArrayResource(decodedResource);
 		}
-		
-		return null;
-	}
 
-	@Override
-	public void onApplicationEvent(ApplicationContextInitializedEvent event) {
-		event.getApplicationContext().addProtocolResolver(this);
+		return null;
 	}
 
 }
