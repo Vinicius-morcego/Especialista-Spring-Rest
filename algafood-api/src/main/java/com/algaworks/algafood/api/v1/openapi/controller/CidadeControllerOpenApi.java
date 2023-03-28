4da195efcd4a5767baa8b1ac9ddbec0071205c6c
unknown <vinicius.souza@novaxs.com.br>
2023-03-28 17:57:27 -0300

    Ajustando a documentação da API para suporte a OAuth2

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -5,7 +5,10 @@ import org.springframework.hateoas.CollectionModel;
 import com.algaworks.algafood.api.v1.model.CidadeModel;
 import com.algaworks.algafood.api.v1.model.input.CidadeInput;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
+
+@SecurityRequirement(name = "security_auth")
 public interface CidadeControllerOpenApi {
 
 	
