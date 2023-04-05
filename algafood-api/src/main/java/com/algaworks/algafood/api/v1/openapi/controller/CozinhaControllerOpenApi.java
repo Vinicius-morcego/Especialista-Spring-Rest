bff2250b3d9f5da7a12c0aee88645ab55a9ac794
unknown <vinicius.souza@novaxs.com.br>
2023-04-04 21:26:19 -0300

    Corrigindo documentação com substituição de Pageable

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CozinhaControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CozinhaControllerOpenApi.java
@@ -8,7 +8,9 @@ import com.algaworks.algafood.api.v1.model.input.CozinhaInput;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -19,8 +21,30 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Cozinhas")
 public interface CozinhaControllerOpenApi {
 
-	@Operation(summary = "Lista as cozinhas")
-	PagedModel<CozinhaModel> listar(Pageable pageable);
+	@Parameter(
+				in = ParameterIn.QUERY,
+				name = "page",
+				description = "Número da página (0...N)",
+				schema = @Schema(type = "integer", defaultValue = "0")
+	)
+	@Parameter(
+			in = ParameterIn.QUERY,
+			name = "size",
+			description = "Quantidade de elementos por página",
+			schema = @Schema(type = "integer", defaultValue = "10")
+	)
+	@Parameter(
+			in = ParameterIn.QUERY,
+			name = "sort",
+			description = "Critérios de ordenação: propriedade(asc|desc)",
+			schema = @Schema(type = "", defaultValue = ""),
+			examples = {
+					@ExampleObject("nome"),
+					@ExampleObject("nome,asc"),
+					@ExampleObject("nome,desc")
+			}
+	)
+	PagedModel<CozinhaModel> listar(@Parameter(hidden = true) Pageable pageable);
 	
 	@Operation(summary = "Lista a cozinha por ID", description = "Lista uma cozinha por ID, "
 			+ "necessita de um ID válido", responses = {
