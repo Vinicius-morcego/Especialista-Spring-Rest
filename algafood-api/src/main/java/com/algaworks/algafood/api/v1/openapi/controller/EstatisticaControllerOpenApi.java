e02a5542df0f21cdc1ad4414c9337c0683b3b9f2
unknown <vinicius.souza@novaxs.com.br>
2023-04-07 15:57:50 -0300

    Desafio: descrevendo documentação de endpoint de estatísticas

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/EstatisticaControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/EstatisticaControllerOpenApi.java
@@ -10,6 +10,8 @@ import com.algaworks.algafood.domain.modelo.dto.VendaDiaria;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -20,19 +22,28 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Estatisticas")
 public interface EstatisticaControllerOpenApi {
 	
+	@Operation(hidden = true)
 	EstatisticasModel estatistica();
 	
-	@Operation(summary = "Lista as vendas diarias pelo filtro", description = "Lista as vendas diárias pelo filtro informado, "
-			+ "necessita de um filtro válido", responses = {
-					@ApiResponse(responseCode = "200"),
-					@ApiResponse(responseCode = "400", description = "ID de cidade inválido",
-							content = @Content(schema = @Schema(ref = "Problem")))
+	@Operation(summary = "Consulta estatisticas de vendas diarias", 
+			parameters = {
+					@Parameter(in = ParameterIn.QUERY, name = "restauranteId", description = "ID do restaurante", 
+						example = "1", schema = @Schema(type = "integer")),
+					@Parameter(in = ParameterIn.QUERY, name = "dataCriacaoInicio", description = "Data/hora inicial de criação do pedido", 
+						example = "2023-04-07T00:00:00Z", schema = @Schema(type = "string", format = "date-time")),
+					@Parameter(in = ParameterIn.QUERY, name = "dataCriacaoFim", description = "Data/hora final de criação do pedido", 
+					example = "2023-04-07T0023:59:59Z", schema = @Schema(type = "string", format = "date-time"))
+	}, responses = {
+			@ApiResponse(responseCode = "200", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = VendaDiaria.class))),
+					@Content(mediaType = "application/pdf", schema = @Schema(type = "string", format = "binary"))
 			})
-	public List<VendaDiaria> consultarVendasDiarias(@Parameter(description = "Representa os critérios da pesquisa", required = true) VendaDiariaFilter filtro, 
-			@Parameter(description = "Representa o tipo do offset", example = "UTC", required = true) String timeOffset);
+	})
+	public List<VendaDiaria> consultarVendasDiarias(@Parameter(hidden = true) VendaDiariaFilter filtro, 
+			@Parameter(description = "Deslocamento de horário a ser considerado na consulta em relação ao UTC", 
+			schema = @Schema(type = "string", defaultValue = "+00:00")) String timeOffset);
 	
-	@Operation(summary = "Lista as vendas diarias pelo filtro e gera em formato PDF", description = "Lista as vendas diárias pelo filtro informado e gera o relatório no formato PDF, "
-			+ "necessita de um filtro válido")
+	@Operation(hidden = true)
 	public ResponseEntity<byte[]> consultarVendasDiariasPdf(@Parameter(description = "Representa os critérios da pesquisa", required = true) VendaDiariaFilter filtro,
 			@Parameter(description = "Representa o tipo do offset", example = "UTC", required = true) String timeOffset);
 }
