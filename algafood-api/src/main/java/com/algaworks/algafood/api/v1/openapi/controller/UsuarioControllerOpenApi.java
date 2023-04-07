d4736460d1dbb522cdd2343db64504edd375d33f
unknown <vinicius.souza@novaxs.com.br>
2023-04-07 15:35:56 -0300

    Desafio: descrevendo documentação de endpoints de usuários

--- algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/UsuarioControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/UsuarioControllerOpenApi.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.algaworks.algafood.api.v1.model.UsuarioModel;
 import com.algaworks.algafood.api.v1.model.input.SenhaInput;
@@ -15,6 +14,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -28,8 +28,8 @@ public interface UsuarioControllerOpenApi {
 	
 	@Operation(summary = "Lista o usuario por ID", description = "Lista um usuario por ID, "
 			+ "necessita de um ID válido", responses = {
-					@ApiResponse(responseCode = "200"),
-					@ApiResponse(responseCode = "400", description = "ID de cidade inválido",
+					@ApiResponse(responseCode = "200", description = "Listagem de usuarios realizada com sucesso"),
+					@ApiResponse(responseCode = "400", description = "Usuário não encontrado",
 							content = @Content(schema = @Schema(ref = "Problem")))
 			})
 	public UsuarioModel buscar(
@@ -37,23 +37,39 @@ public interface UsuarioControllerOpenApi {
 			@PathVariable Long usuarioId);	 
 	
 	@Operation(summary = "Cadastro do usuario", description = "Cadastro de um usuario, "
-			+ "necessita de um login e uma senha válidos")
-	public UsuarioModel salvar(@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Representação de um novo usuario") 
-	@RequestBody @Valid UsuarioComSenhaInput usuarioInput);	
+			+ "necessita de um login e uma senha válidos", responses = {
+					@ApiResponse(responseCode = "200", description = "Usuario cadastrado com sucesso"),
+					@ApiResponse(responseCode = "400", description = "Usuário não encontrado",
+							content = @Content(schema = @Schema(ref = "Problem")))
+			})
+	public UsuarioModel salvar(@RequestBody(description = "Representação de um novo usuario") 
+		@Valid UsuarioComSenhaInput usuarioInput);	
 	
 	@Operation(summary = "Atualiza o usuario por ID", description = "Atualização de um usuario por ID, "
-			+ "necessita de um ID válido")
+			+ "necessita de um ID válido", responses = {
+					@ApiResponse(responseCode = "200", description = "Usuario atualizado com sucesso"),
+					@ApiResponse(responseCode = "400", description = "Usuário não encontrado",
+							content = @Content(schema = @Schema(ref = "Problem")))
+			})
 	public UsuarioModel atualizar(@Parameter(description = "Representa o ID de um usuario", example = "1", required = true) Long usuarioId, 
-			@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Representação de um usuario com dados atualizados")
+			@RequestBody(description = "Representação de um usuario com dados atualizados")
 			UsuarioInput usuarioInput);
 	
 	@Operation(summary = "Atualiza a senha do usuario por ID", description = "Atualização de senha de um usuario por ID, "
-			+ "necessita de um ID válido")
+			+ "necessita de um ID válido", responses = {
+					@ApiResponse(responseCode = "204", description = "Senha do usuario atualizada com sucesso"),
+					@ApiResponse(responseCode = "400", description = "Usuário não encontrado",
+							content = @Content(schema = @Schema(ref = "Problem")))
+			})
 	public void atualizarSenha(@Parameter(description = "Representa o ID de um usuario", example = "1", required = true) Long usuarioId, 
-			@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Representação de uma nova senha do usuario", required = true) 
+			@RequestBody(description = "Representação de uma nova senha do usuario", required = true) 
 			SenhaInput senhaInput);
 	
 	@Operation(summary = "Remove o usuario por ID", description = "Remoção de um usuario por ID, "
-			+ "necessita de um ID válido")
+			+ "necessita de um ID válido", responses = {
+					@ApiResponse(responseCode = "204", description = "Usuário removido com sucesso"),
+					@ApiResponse(responseCode = "400", description = "Usuário não encontrado",
+							content = @Content(schema = @Schema(ref = "Problem")))
+			})
 	public void remover(@Parameter(description = "Representa o ID de um usuario", example = "1", required = true) Long usuarioId);
 }
